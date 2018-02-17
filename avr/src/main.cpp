557c121895cef35cf62dc6265ac113cd9b8b95f9
Neil Movva <neilmovva97@gmail.com>
2018-02-17 01:27:38 -0800

    finished port to avr, B1 board accepting commands over uart

--- avr/src/main.cpp
+++ avr/src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
+#include "mini-printf.h"
 
-const uint32_t PERIOD_VALUE  =  (256 - 1);
+const uint8_t MAX_PWM_LEVEL  =  100;
 #define DRV_INVERTED
 
+#define DDR_LED    	DDRC
 #define PORT_LED    PORTC
 #define PIN_LED_R   0
 
@@ -11,10 +13,13 @@ const uint32_t PERIOD_VALUE  =  (256 - 1);
 #define PIN_PWM_CH1     1
 #define PIN_PWM_CH2     2
 
-#define PORT_IO_CH3    PORTD
-#define PIN_IO_CH3     5
+#define PORT_IO_CH3    	PORTD
+#define PIN_IO_CH3     	5
 #define NUM_CHANNELS 	3
 
+//helper macros
+#define SET(x,y) (x |= (1<<y))
+#define CLR(x,y) (x &= (~(1<<y)))
 
 //SSSP = Sail Simple Serial Protocol
 struct sssp_packet_pwm {
@@ -30,6 +35,29 @@ const char* BOOKEND_FTR  	=	"rsch";
 
 #define BAUDRATE 				38400
 #define ENTER_CHAR 				0x0D
+
+bool char_is_alphanumeric(const char c) {
+	bool is_num = (c >= 48U) && (c <= 57U);
+	bool is_uppercase = (c >= 65U) && (c <= 90U);
+	bool is_lowercase = (c >= 97U) && (c <= 122U);
+	return (is_num || is_uppercase || is_lowercase);
+}
+
+uint8_t parse_pwm_level(const char* str_pwmlevel) {
+    uint8_t d_hun = str_pwmlevel[0] - '0'; 
+	uint8_t d_ten = str_pwmlevel[1] - '0'; 
+	uint8_t d_one = str_pwmlevel[2] - '0';
+
+	uint8_t result = d_hun*100 + d_ten*10 + d_one*1;
+	return result;
+}
+
+uint8_t parse_pwm_ch(const char* str_pwmch) {
+	uint8_t d_one = str_pwmch[0] - '0';
+
+	uint8_t result = d_one*1;
+	return result;
+}
 
 
 void sssp_process_packet_pwm(sssp_packet_pwm_t* pkt) {
@@ -60,12 +88,12 @@ void sssp_process_packet_pwm(sssp_packet_pwm_t* pkt) {
 	strncat(str_pwm_channel, pkt->pwm_channel, 
 		sizeof(pkt->pwm_channel));
 	//parse text data
-	uint32_t pwm_level = myAtoi(str_pwm_level);
-	uint32_t pwm_channel = myAtoi(str_pwm_channel);
+	uint8_t pwm_level = parse_pwm_level(str_pwm_level);
+	uint8_t pwm_channel = parse_pwm_ch(str_pwm_channel);
 	bool command_is_sane = true;
 
-	if(pwm_level > PERIOD_VALUE) {
-		Serial.println("error   -- PWM value set too high, ignoring");
+	if(pwm_level > MAX_PWM_LEVEL) {
+ 		Serial.println("error   -- PWM value set too high, ignoring");
 		command_is_sane = false;
 	} 
 	if (pwm_channel > NUM_CHANNELS) {
@@ -75,13 +103,13 @@ void sssp_process_packet_pwm(sssp_packet_pwm_t* pkt) {
 
 	//if a valid command, commit payload data to HW
 	if(command_is_sane) {
-		pwm_update_ch(pwm_level, pwm_channel);
+		// TODO pwm_update_ch(pwm_level, pwm_channel);
 		Serial.println("committed");
 		Serial.print("    "); 	//indent sucessful commands
 	}
 	
 	//send acknowledgement string
-	const int bufsz = 64;
+	const uint8_t bufsz = 64;
 	char receipt[bufsz];
 	mini_snprintf(receipt, bufsz, "CH: %u  PWR: %u", 
 		pwm_channel, pwm_level);
@@ -92,18 +120,21 @@ void sssp_process_packet_pwm(sssp_packet_pwm_t* pkt) {
 void sssp_receive_loop() {
 	char buffer_rx[sizeof(sssp_packet_pwm_t) + 1];
 	buffer_rx[sizeof(sssp_packet_pwm_t)] = '\0';
-	uint32_t buffer_idx = 0;
+	uint8_t buffer_idx = 0;
 
-	uint32_t state_current = 0;
+	uint8_t state_current = 0;
 	while(1) {
-		char input_char = usart_recv_blocking(USART1);
+		//blocking busywait for serial data
+		while(Serial.available() < 1) {};
+		
+		char input_char = Serial.read();
 		
 		bool should_accept_char = false;
 		bool should_reset_state = false;
 
 		if(char_is_alphanumeric(input_char)) {
 			//echo char
-			usart_send_blocking(USART1, input_char);
+			Serial.print(input_char);
 
 			//watch for header sequence
 			if(state_current < strlen(BOOKEND_HDR)) {
@@ -124,26 +155,26 @@ void sssp_receive_loop() {
 
 		} else if(input_char == ENTER_CHAR) {
 			//visually end user input with NL+CR
-			usart_newln();
+			Serial.println();
 			should_reset_state = true;
 		}
 
 		if(should_accept_char) {
-			gpio_set(PORT_LED, PIN_LED);
+			SET(PORT_LED, PIN_LED_R);
 			buffer_rx[buffer_idx] = input_char;
 			buffer_idx++;
 			//once buffer is full, ship pkt downstream
 			if(buffer_idx >= sizeof(sssp_packet_pwm_t)) {
 				sssp_packet_pwm_t pkt[1];
 				memcpy(pkt, buffer_rx, sizeof(sssp_packet_pwm_t));
-				usart_newln();
+				Serial.println();
 				sssp_process_packet_pwm(pkt);
 				should_reset_state = true;
 			}
 		}
 
 		if(should_reset_state) {
-			gpio_clear(PORT_LED, PIN_LED);
+			CLR(PORT_LED, PIN_LED_R);
 			//reset state and buffer
 			state_current = 0;
 			buffer_idx = 0;
@@ -154,28 +185,20 @@ void sssp_receive_loop() {
 	}
 }
 
-int main(void) {
-    rcc_clock_setup_in_hsi_out_48mhz();
-    gpio_setup();
-	pwm_setup();
-	usart_setup();
-
-    for(int iter_blink = 0; iter_blink < 8; iter_blink++) {
-		gpio_toggle(PORT_LED, PIN_LED);
-		busywait_ms(25);
-    }
-	
-	sssp_receive_loop();
-
-	return 0;
-}
-
-
-
 void setup() {
-    // put your setup code here, to run once:
+	SET(DDR_LED, PIN_LED_R);
+
+    for(int iter_blink = 0; iter_blink < (4 * 5); iter_blink++) {
+		SET(PORT_LED, PIN_LED_R);
+		_delay_ms(100);
+		CLR(PORT_LED, PIN_LED_R);
+		_delay_ms(100);
+    }
+
+	Serial.begin(BAUDRATE);
+	Serial.println("hello world");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+	sssp_receive_loop();
 }
