7ac209a7a7bad8bed4a6494951d14ea39e152286
Neil Movva <neilmovva97@gmail.com>
2018-02-18 03:14:23 -0800

    sync

--- avr/src/main.cpp
+++ avr/src/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
-#include <TimerOne.h>
 #include "mini-printf.h"
 
 #define DRV_INVERTED
 
-const uint16_t PWM_PERIOD 	  		=  100;
+const uint16_t PWM_PERIOD_CYCLES 	=  100;
+const uint16_t PWM_VALUE_MAX 		=  PWM_PERIOD_CYCLES;
 const uint8_t  SSSP_MAX_PWR_LEVEL  	=  100;
 #ifdef DRV_INVERTED
-const uint16_t  REST_PWM_LEVEL 		=  1023;
+const uint16_t  REST_PWMLEVEL		=  PWM_VALUE_MAX;
 #else 
-const uint8_t REST_PWM_LEVEL =  0;
+const uint8_t REST_PWMLEVEL =  0;
 #endif
 
 #define DDR_LED    	DDRC
@@ -18,6 +18,7 @@ const uint8_t REST_PWM_LEVEL =  0;
 
 #define NUM_CHANNELS 	3
 
+#define DDR_PWM 		DDRB
 #define PORT_PWM_CH1    PORTB
 #define PORT_PWM_CH2    PORTB
 #define PIN_PWM_CH1     1
@@ -37,8 +38,8 @@ const uint8_t REST_PWM_LEVEL =  0;
 //SSSP = Sail Simple Serial Protocol
 struct sssp_packet_pwm {
 	char hdr[4];
-	char pwm_level[3];
-	char pwm_channel[1];
+	char powerlevel[3];
+	char channel[1];
 	char ftr[4];
 };
 typedef sssp_packet_pwm sssp_packet_pwm_t; 
@@ -56,7 +57,7 @@ bool char_is_alphanumeric(const char c) {
 	return (is_num || is_uppercase || is_lowercase);
 }
 
-uint8_t parse_pwm_level(const char* str_pwmlevel) {
+uint8_t parse_powerlevel(const char* str_pwmlevel) {
     uint8_t d_hun = str_pwmlevel[0] - '0'; 
 	uint8_t d_ten = str_pwmlevel[1] - '0'; 
 	uint8_t d_one = str_pwmlevel[2] - '0';
@@ -65,20 +66,20 @@ uint8_t parse_pwm_level(const char* str_pwmlevel) {
 	return result;
 }
 
-uint8_t parse_pwm_ch(const char* str_pwmch) {
+uint8_t parse_channel(const char* str_pwmch) {
 	uint8_t d_one = str_pwmch[0] - '0';
 
 	uint8_t result = d_one*1;
 	return result;
 }
 
-void pwm_update_ch(uint8_t power_level, uint8_t pwm_channel) {
-	uint16_t pwm_level = 10 * power_level;
+void pwm_update_ch(uint8_t powerlevel, uint8_t channel) {
+	uint8_t pwm_level = 10 * powerlevel;
 	#ifdef DRV_INVERTED
-	pwm_level = 1023 - pwm_level;
+	pwm_level = PWM_VALUE_MAX - pwm_level;
 	#endif
 
-	switch(pwm_channel) {
+	switch(channel) {
 		case 1:
 		Timer1.setPwmDuty(ARDPIN_PWM_CH1, pwm_level);
 		break;
@@ -88,7 +89,7 @@ void pwm_update_ch(uint8_t power_level, uint8_t pwm_channel) {
 		break;
 
 		case 3:
-		if(power_level > 50) {
+		if(pwm_level > 50) {
 			CLR(PORT_IO_CH3, PIN_IO_CH3);
 		} else {
 			SET(PORT_IO_CH3, PIN_IO_CH3);
@@ -119,29 +120,29 @@ void sssp_process_packet_pwm(sssp_packet_pwm_t* pkt) {
 	}
 
 	//unpack payload strings
-	char str_pwm_level[sizeof(pkt->pwm_level) + 1] = {}; 
-	char str_pwm_channel[sizeof(pkt->pwm_channel) + 1] = {}; 
-	strncat(str_pwm_level, pkt->pwm_level, 
-		sizeof(pkt->pwm_level));
-	strncat(str_pwm_channel, pkt->pwm_channel, 
-		sizeof(pkt->pwm_channel));
+	char str_powerlevel[sizeof(pkt->powerlevel) + 1] = {}; 
+	char str_channel[sizeof(pkt->channel) + 1] = {}; 
+	strncat(str_powerlevel, pkt->powerlevel, 
+		sizeof(pkt->powerlevel));
+	strncat(str_channel, pkt->channel, 
+		sizeof(pkt->channel));
 	//parse text data
-	uint8_t pwm_level = parse_pwm_level(str_pwm_level);
-	uint8_t pwm_channel = parse_pwm_ch(str_pwm_channel);
+	uint8_t powerlevel = parse_powerlevel(str_powerlevel);
+	uint8_t channel = parse_channel(str_channel);
 	bool command_is_sane = true;
 
-	if(pwm_level > SSSP_MAX_PWR_LEVEL) {
+	if(powerlevel > SSSP_MAX_PWR_LEVEL) {
  		Serial.println("error   -- PWM value set too high, ignoring");
 		command_is_sane = false;
 	} 
-	if (pwm_channel > NUM_CHANNELS) {
+	if (channel > NUM_CHANNELS) {
 		Serial.println("error   -- target channel not found, ignoring");
 		command_is_sane = false;
 	}
 
 	//if a valid command, commit payload data to HW
 	if(command_is_sane) {
-		pwm_update_ch(pwm_level, pwm_channel);
+		pwm_update_ch(powerlevel, channel);
 		Serial.println("committed");
 		Serial.print("    "); 	//indent sucessful commands
 	}
@@ -150,7 +151,7 @@ void sssp_process_packet_pwm(sssp_packet_pwm_t* pkt) {
 	const uint8_t bufsz = 64;
 	char receipt[bufsz];
 	mini_snprintf(receipt, bufsz, "CH: %u  PWR: %u", 
-		pwm_channel, pwm_level);
+		channel, powerlevel);
 	Serial.println(receipt);
 }
 
@@ -176,7 +177,8 @@ void sssp_receive_loop() {
 		} else if(input_char == ENTER_CHAR) {
 			//visually end user input with NL+CR
 			Serial.println();
-			
+			//optional: reset input buffer
+			return;
 		}
 	}
 	//once buffer is full, ship pkt downstream
@@ -189,7 +191,9 @@ void sssp_receive_loop() {
 void setup() {
 	SET(DDR_LED, PIN_LED_R);
 
-    for(int iter_blink = 0; iter_blink < (4 * 5); iter_blink++) {
+
+	const uint8_t blink_seconds = 3;
+    for(uint8_t iter_blink = 0; iter_blink < (blink_seconds * 5); iter_blink++) {
 		SET(PORT_LED, PIN_LED_R);
 		_delay_ms(100);
 		CLR(PORT_LED, PIN_LED_R);
@@ -199,9 +203,17 @@ void setup() {
 	Serial.begin(BAUDRATE);
 	Serial.println("hello world");
 
-	Timer1.initialize(PWM_PERIOD);
-	Timer1.pwm(ARDPIN_PWM_CH1, REST_PWM_LEVEL); 
-	Timer1.pwm(ARDPIN_PWM_CH2, REST_PWM_LEVEL);
+	// Timer1 setup
+	//Normal mode, overflow at MAX
+	TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);     //WGM 14, FastPWM, TOP=ICR1
+	TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10); //1x prescaler == 16MHz tick
+	ICR1 = PWM_PERIOD_CYCLES;
+	TCNT1  = 0x0000;
+
+	OCR1A = REST_PWMLEVEL;
+	OCR1B = REST_PWMLEVEL;
+	SET(DDR_PWM, PIN_PWM_CH1);
+	SET(DDR_PWM, PIN_PWM_CH2);
 
 	SET(DDR_IO_CH3, PIN_IO_CH3);
 	#ifdef DRV_INVERTED
