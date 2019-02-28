33fc54b844440ba4800856080b16780c97b845a5
Neil Movva <neilmovva97@gmail.com>
2019-02-28 12:35:23 -0800

    break out sssp protocol into header lib

---
+++ avr/lib/sssp/sssp.h
@@ -0,0 +1,35 @@
+//SSSP = Sail Simple Serial Protocol
+struct sssp_packet_pwm {
+	char hdr[4];
+	char powerlevel[3];
+	char channel[1];
+	char ftr[4];
+};
+typedef sssp_packet_pwm sssp_packet_pwm_t; 
+
+const char* BOOKEND_HDR 	=	"sail";
+const char* BOOKEND_FTR  	=	"rsch";
+
+
+bool char_is_alphanumeric(const char c) {
+	bool is_num = (c >= 48U) && (c <= 57U);
+	bool is_uppercase = (c >= 65U) && (c <= 90U);
+	bool is_lowercase = (c >= 97U) && (c <= 122U);
+	return (is_num || is_uppercase || is_lowercase);
+}
+
+uint8_t parse_powerlevel(const char* str_pwmlevel) {
+    uint8_t d_hun = str_pwmlevel[0] - '0'; 
+	uint8_t d_ten = str_pwmlevel[1] - '0'; 
+	uint8_t d_one = str_pwmlevel[2] - '0';
+
+	uint8_t result = d_hun*100 + d_ten*10 + d_one*1;
+	return result;
+}
+
+uint8_t parse_channel(const char* str_pwmch) {
+	uint8_t d_one = str_pwmch[0] - '0';
+
+	uint8_t result = d_one*1;
+	return result;
+}
--- avr/src/main.cpp
+++ avr/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include "mini-printf.h"
+#include "sssp.h"
 
 #include "sinetable.h"
 #define SINETABLE sin_a255_p4096
@@ -51,48 +52,15 @@ const uint8_t REST_PWMLEVEL =  0;
 #define PIN_PWM_CH1     1
 #define PIN_PWM_CH2     2
 
+
 #define DDR_PWM_CH3 	DDRD
 #define PORT_PWM_CH3    PORTD
 #define PIN_PWM_CH3     5
 
 
-
-//SSSP = Sail Simple Serial Protocol
-struct sssp_packet_pwm {
-	char hdr[4];
-	char powerlevel[3];
-	char channel[1];
-	char ftr[4];
-};
-typedef sssp_packet_pwm sssp_packet_pwm_t; 
-
-const char* BOOKEND_HDR 	=	"sail";
-const char* BOOKEND_FTR  	=	"rsch";
-
 #define BAUDRATE 				38400
 #define ENTER_CHAR 				0x0D
 
-bool char_is_alphanumeric(const char c) {
-	bool is_num = (c >= 48U) && (c <= 57U);
-	bool is_uppercase = (c >= 65U) && (c <= 90U);
-	bool is_lowercase = (c >= 97U) && (c <= 122U);
-	return (is_num || is_uppercase || is_lowercase);
-}
-
-uint8_t parse_powerlevel(const char* str_pwmlevel) {
-    uint8_t d_hun = str_pwmlevel[0] - '0'; 
-	uint8_t d_ten = str_pwmlevel[1] - '0'; 
-	uint8_t d_one = str_pwmlevel[2] - '0';
-
-	uint8_t result = d_hun*100 + d_ten*10 + d_one*1;
-	return result;
-}
-
-uint8_t parse_channel(const char* str_pwmch) {
-	uint8_t d_one = str_pwmch[0] - '0';
-	uint8_t result = d_one*1;
-	return result;
-}
 
 // HAL function, verify against hardware!
 void pwm_ch_disable(uint8_t channel) {
